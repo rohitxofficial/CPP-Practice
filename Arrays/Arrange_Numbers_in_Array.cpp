3ef0bc3ef72db88913b8184cc37746eecedd916b
Rohit Panchal <61163331+rohitxofficial@users.noreply.github.com>
2021-07-25 01:15:02 +0530

    Update Arrange_Numbers_in_Array.cpp

--- Arrays/Arrange_Numbers_in_Array.cpp
+++ Arrays/Arrange_Numbers_in_Array.cpp
@@ -1,3 +1,10 @@
+/*
+You have been given an empty array(arr) and its size n.
+The only input taken from the user will be n and 
+you need not worry about the array. Your task is to populate the array
+using the integer values in the range 1 to N(both inclusive) in the order (1,3,5,.......6,4,2).
+*/
+
 #include <iostream>
 using namespace std;
 
