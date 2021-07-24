d9acb5cf64157a800cddbca6a80d7901e793cf95
Rohit Panchal <61163331+rohitxofficial@users.noreply.github.com>
2021-07-25 00:57:55 +0530

    Add files via upload

---
+++ Arrays/Find_Unique.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        int count = 0;
+        for (int j = 0; j < n; j++)
+        {
+            if (i == j)
+            {
+                continue;
+            }
+            else
+            {
+                if (arr[i] == arr[j])
+                {
+                    count++;
+                }
+            }
+        }
+        if (count == 0)
+        {
+            cout << arr[i] << endl;
+        }
+    }
+
+    return 0;
+}
