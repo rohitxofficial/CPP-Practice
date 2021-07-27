67208d79eaded5dfdec286838e848bb995cc08a1
Rohit Panchal <61163331+rohitxofficial@users.noreply.github.com>
2021-07-27 18:47:14 +0530

    Add files via upload

---
+++ Searching and Sorting/Insertion_Sort.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+void PrintArray(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+void InsertionSort(int arr[], int n)
+{
+    for (int i = 1; i < n; i++)
+    {
+        int key = arr[i];
+        int j = i - 1;
+        while (key < arr[j] && j >= 0)
+        {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = key;
+    }
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    InsertionSort(arr, n);
+
+    PrintArray(arr, n);
+
+    return 0;
+}
