bae3eadcd3c4f832bee5cc4235966209edf811bf
Rohit Panchal <61163331+rohitxofficial@users.noreply.github.com>
2021-10-05 17:31:41 +0530

    Update Array_Intersection.cpp

--- Arrays/Array_Intersection.cpp
+++ Arrays/Array_Intersection.cpp
@@ -1,44 +1,102 @@
-/*
-Given two integer arrays(arr1 and arr2) of size m and n, respectively. 
-You need to print their intersection; An intersection for this problem 
-can be defined when both the arrays contain a particular value or to put
-it in other words, when there is a common value that exists in both the arrays.
-*/
-
 #include <iostream>
-#include <climits>
 using namespace std;
+
+int partition(int arr[], int st, int en)
+{
+    int pivot = arr[st];
+
+    int smallCount = 0;
+
+    for (int i = st; i <= en; i++)
+    {
+        if (arr[i] < pivot)
+            smallCount++;
+    }
+
+    int temp = arr[st + smallCount];
+    arr[st + smallCount] = pivot;
+    arr[st] = temp;
+
+    int p1 = st;
+    int p2 = en;
+    while (p1 < st + smallCount && p2 > st + smallCount)
+    {
+        if (arr[p1] < pivot)
+        {
+            p1++;
+        }
+        else if (arr[p2] >= pivot)
+        {
+            p2--;
+        }
+        else
+        {
+            int temp2 = arr[p2];
+            arr[p2] = arr[p1];
+            arr[p1] = temp2;
+        }
+    }
+
+    return st + smallCount;
+}
+
+void sort(int arr[], int st, int en)
+{
+    if (st >= en)
+    {
+        return;
+    }
+
+    int pivot_pos = partition(arr, st, en);
+    sort(arr, st, pivot_pos - 1);
+    sort(arr, pivot_pos + 1, en);
+}
 
 int main()
 {
-    int n;
-    cin >> n;
-    int arr1[n];
-    for (int i = 0; i < n; i++)
+    int n1;
+    cin >> n1;
+
+    int *arr1 = new int[n1];
+
+    for (int i = 0; i < n1; i++)
     {
         cin >> arr1[i];
     }
 
-    int m;
-    cin >> m;
-    int arr2[m];
-    for (int i = 0; i < m; i++)
+    int n2;
+    cin >> n2;
+
+    int *arr2 = new int[n2];
+
+    for (int i = 0; i < n2; i++)
     {
         cin >> arr2[i];
     }
 
-    for (int i = 0; i < n; i++)
+    sort(arr1, 0, n1 - 1);
+    sort(arr2, 0, n2 - 1);
+
+    int p1 = 0;
+    int p2 = 0;
+
+    while (p1 < n1 && p2 < n2)
     {
-        for (int j = 0; j < m; j++)
+        if (arr1[p1] > arr2[p2])
+            p2++;
+        else if (arr1[p1] < arr2[p2])
+            p1++;
+        else
         {
-            if (arr1[i] == arr2[j])
-            {
-                cout << arr2[j] << " ";
-                arr2[j] = INT_MIN;
-                break;
-            }
+            cout << arr1[p1] << " ";
+            p1++;
+            p2++;
         }
     }
+    cout << endl;
+
+    delete arr1;
+    delete arr2;
 
     return 0;
 }
