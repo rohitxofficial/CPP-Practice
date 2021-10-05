5211cea1c1c6785731d14fe5ea2b3d764a9770b3
Rohit Panchal <61163331+rohitxofficial@users.noreply.github.com>
2021-10-05 14:58:55 +0530

    Update Find_Duplicate.cpp

--- Arrays/Find_Duplicate.cpp
+++ Arrays/Find_Duplicate.cpp
@@ -1,42 +1,28 @@
-/*
-Given an integer array(arr) of size n which contains numbers from 0 to (N - 2).
-Each number is present at least once. That is, if n = 5, the array/list constitutes 
-values ranging from 0 to 3 and among these, there is a single integer value that is present twice. 
-You need to find and return that duplicate number present in the array.
-*/
-
 #include <iostream>
 using namespace std;
 
 int main()
 {
-    int t;
-    cin >> t;
-    for (int i = 0; i < t; i++)
+    int n;
+    cin >> n;
+
+    int arr[n];
+
+    for (int i = 0; i < n; i++)
     {
-        int n;
-        cin >> n;
-
-        int arr[n];
-        for (int j = 0; j < n; j++)
-        {
-            cin >> arr[j];
-        }
-
-        int duplicate[n - 1] = {0};
-
-        for (int j = 0; j < n; j++)
-        {
-            duplicate[arr[j]]++;
-        }
-
-        for (int j = 0; j < n - 1; j++)
-        {
-            if (duplicate[j] == 2)
-            {
-                cout << j << endl;
-                break;
-            }
-        }
+        cin >> arr[i];
     }
+
+    int sum_array = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        sum_array += arr[i];
+    }
+
+    int sum = ((n - 2) * (n - 1)) / 2;
+
+    cout << sum_array - sum << endl;
+
+    return 0;
 }
