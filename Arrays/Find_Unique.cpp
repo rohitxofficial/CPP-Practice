ad0d096dd5d011001218daf174b5ceab766f89ee
Rohit Panchal <61163331+rohitxofficial@users.noreply.github.com>
2021-10-05 13:27:22 +0530

    Update Find_Unique.cpp

--- Arrays/Find_Unique.cpp
+++ Arrays/Find_Unique.cpp
@@ -1,9 +1,3 @@
-/*
-Given an integer array(arr) of size n. Where n is equal to [2m + 1].
-Now, in the given array, 'm' numbers are present twice and one number is present only once.
-You need to find and return that number which is unique in the array.
-*/
-
 #include <iostream>
 using namespace std;
 
@@ -11,34 +5,22 @@ int main()
 {
     int n;
     cin >> n;
+
     int arr[n];
+
     for (int i = 0; i < n; i++)
     {
         cin >> arr[i];
     }
 
-    for (int i = 0; i < n; i++)
+    int ans = arr[0];
+
+    for (int i = 1; i < n; i++)
     {
-        int count = 0;
-        for (int j = 0; j < n; j++)
-        {
-            if (i == j)
-            {
-                continue;
-            }
-            else
-            {
-                if (arr[i] == arr[j])
-                {
-                    count++;
-                }
-            }
-        }
-        if (count == 0)
-        {
-            cout << arr[i] << endl;
-        }
+        ans = ans ^ arr[i];
     }
+
+    cout << ans << endl;
 
     return 0;
 }
