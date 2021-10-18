6c0dace62c1d17bd54e63a0c53d42cf5ad9c93f8
Rohit Panchal <61163331+rohitxofficial@users.noreply.github.com>
2021-10-18 19:55:24 +0530

    Update Merge_Two_Sorted_LinkedLists.cpp

--- Linked List/Merge_Two_Sorted_LinkedLists.cpp
+++ Linked List/Merge_Two_Sorted_LinkedLists.cpp
@@ -53,21 +53,6 @@ void printLL(Node *head)
     cout << endl;
 }
 
-void placeNode(Node *&head, Node *&tail, Node *&head1)
-{
-    if (head == NULL)
-    {
-        head = head1;
-        tail = head1;
-    }
-    else
-    {
-        tail->next = head1;
-        tail = head1;
-    }
-    head1 = head1->next;
-}
-
 int main()
 {
     Node *head1 = takeInput();
@@ -76,25 +61,68 @@ int main()
     Node *head = NULL;
     Node *tail = NULL;
 
+    if (head1 != NULL)
+    {
+        if (head2 != NULL)
+        {
+            if (head1->data > head2->data)
+            {
+                head = head2;
+                tail = head2;
+                head2 = head2->next;
+            }
+            else
+            {
+                head = head1;
+                tail = head1;
+                head1 = head1->next;
+            }
+        }
+        else
+        {
+            head = head1;
+            tail = head1;
+            head1 = head1->next;
+        }
+    }
+    else if (head2 != NULL)
+    {
+        head = head2;
+        tail = head2;
+        head2 = head2->next;
+    }
+    else
+    {
+        return 0;
+    }
+
     while (head1 != NULL || head2 != NULL)
     {
         if (head1 == NULL)
         {
-            placeNode(head, tail, head2);
+            tail->next = head2;
+            break;
         }
         else if (head2 == NULL)
         {
-            placeNode(head, tail, head1);
+            tail->next = head1;
+            break;
         }
         else
         {
             if (head1->data > head2->data)
             {
-                placeNode(head, tail, head2);
+                tail->next = head2;
+                tail = head2;
+
+                head2 = head2->next;
             }
             else
             {
-                placeNode(head, tail, head1);
+                tail->next = head1;
+                tail = head1;
+
+                head1 = head1->next;
             }
         }
     }
