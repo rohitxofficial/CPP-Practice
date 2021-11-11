c4c1817bfbc2e1e94714343f39226338bea20d23
Rohit Panchal <61163331+rohitxofficial@users.noreply.github.com>
2021-11-11 20:56:55 +0530

    Add files via upload

---
+++ Trees/PostOrder_Traversal.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+template <typename T>
+class TreeNode
+{
+public:
+    T data;
+    vector<TreeNode<T> *> children;
+
+    TreeNode(T data)
+    {
+        this->data = data;
+    }
+};
+
+TreeNode<int> *takeInput()
+{
+    queue<TreeNode<int> *> pendingNodes;
+
+    int data;
+    cout << "Enter root data: ";
+    cin >> data;
+
+    TreeNode<int> *root = new TreeNode<int>(data);
+
+    pendingNodes.push(root);
+
+    while (pendingNodes.size() != 0)
+    {
+        TreeNode<int> *front = pendingNodes.front();
+        pendingNodes.pop();
+
+        cout << "Enter num of children of " << front->data << ": ";
+        int numChild;
+        cin >> numChild;
+        for (int i = 0; i < numChild; i++)
+        {
+            int childData;
+            cout << "Enter " << i << "th child of " << front->data << endl;
+            cin >> childData;
+
+            TreeNode<int> *childNode = new TreeNode<int>(childData);
+            front->children.push_back(childNode);
+            pendingNodes.push(childNode);
+        }
+    }
+    return root;
+}
+
+void postOrder(TreeNode<int> *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+
+    for (int i = 0; i < root->children.size(); i++)
+    {
+        postOrder(root->children[i]);
+    }
+
+    cout << root->data << " ";
+}
+
+int main()
+{
+    TreeNode<int> *root = takeInput();
+
+    postOrder(root);
+
+    cout << endl;
+
+    return 0;
+}
