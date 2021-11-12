d74788838ad2066eeff03283e05668d49653b9b9
Rohit Panchal <61163331+rohitxofficial@users.noreply.github.com>
2021-11-13 01:38:17 +0530

    Add files via upload

---
+++ Binary Trees/BinaryTreeNode.h
@@ -0,0 +1,20 @@
+template <typename T>
+class BinaryTreeNode
+{
+public:
+    T data;
+    BinaryTreeNode<T> *left;
+    BinaryTreeNode<T> *right;
+
+    BinaryTreeNode(T data)
+    {
+        this->data = data;
+        left = right = NULL;
+    }
+
+    ~BinaryTreeNode()
+    {
+        delete left;
+        delete right;
+    }
+};
