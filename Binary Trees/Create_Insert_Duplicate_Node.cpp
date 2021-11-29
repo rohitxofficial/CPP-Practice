9a290f49d98026d4cb8b01caf3a3f62d99b0decd
Rohit Panchal <61163331+rohitxofficial@users.noreply.github.com>
2021-11-29 20:02:18 +0530

    Add files via upload

---
+++ Binary Trees/Create_Insert_Duplicate_Node.cpp
@@ -0,0 +1,105 @@
+#include <bits/stdc++.h>
+#include "BinaryTreeNode.h"
+using namespace std;
+
+BinaryTreeNode<int> *takeInput()
+{
+    int data;
+    cin >> data;
+
+    queue<BinaryTreeNode<int> *> q;
+
+    BinaryTreeNode<int> *root = NULL;
+
+    if (data != -1)
+    {
+        root = new BinaryTreeNode<int>(data);
+        q.push(root);
+    }
+
+    while (!q.empty())
+    {
+        BinaryTreeNode<int> *front = q.front();
+        q.pop();
+
+        int leftData, rightData;
+
+        cin >> leftData >> rightData;
+
+        BinaryTreeNode<int> *left = NULL, *right = NULL;
+
+        if (leftData != -1)
+        {
+            left = new BinaryTreeNode<int>(leftData);
+            q.push(left);
+        }
+        if (rightData != -1)
+        {
+            right = new BinaryTreeNode<int>(rightData);
+            q.push(right);
+        }
+
+        front->left = left;
+        front->right = right;
+    }
+
+    return root;
+}
+
+void printBinaryTree(BinaryTreeNode<int> *root)
+{
+    if (root == NULL)
+        return;
+
+    queue<BinaryTreeNode<int> *> q;
+    q.push(root);
+    q.push(NULL);
+    while (!q.empty())
+    {
+        BinaryTreeNode<int> *front = q.front();
+        q.pop();
+
+        if (front == NULL)
+        {
+            cout << endl;
+            if (!q.empty())
+                q.push(NULL);
+        }
+        else
+        {
+            cout << front->data << " ";
+
+            if (front->left != NULL)
+                q.push(front->left);
+
+            if (front->right != NULL)
+                q.push(front->right);
+        }
+    }
+}
+
+void createInsertDuplicate(BinaryTreeNode<int> *root)
+{
+    if (root == NULL)
+        return;
+
+    BinaryTreeNode<int> *temp = root->left;
+
+    BinaryTreeNode<int> *newNode = new BinaryTreeNode<int>(root->data);
+
+    root->left = newNode;
+
+    newNode->left = temp;
+
+    createInsertDuplicate(newNode->left);
+    createInsertDuplicate(root->right);
+}
+
+int main()
+{
+    BinaryTreeNode<int> *root = takeInput();
+
+    createInsertDuplicate(root);
+
+    printBinaryTree(root);
+}
