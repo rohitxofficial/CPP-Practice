f291af2febf02112b60383e82bc48cae788d7441
Rohit Panchal <61163331+rohitxofficial@users.noreply.github.com>
2021-11-30 03:07:21 +0530

    Add files via upload

---
+++ Hashmaps/Remove_Duplicates.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    int *arr = new int[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    vector<int> ans;
+    unordered_map<int, bool> myMap;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (myMap.count(arr[i]) == 0)
+        {
+            myMap[arr[i]] = true;
+            ans.push_back(arr[i]);
+        }
+        else
+        {
+            continue;
+        }
+    }
+
+    for (int i = 0; i < ans.size(); i++)
+    {
+        cout << ans[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
